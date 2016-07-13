4cd503bc9da59a32ffc2431ca73cf7de20d090b5
Elifarley C <elifarley@users.noreply.github.com>
2016-07-13 19:09:32 -0300

    Create random user and password on every launch

--- app.sh
+++ app.sh
@@ -2,4 +2,9 @@
 test -L /data/activemq.log -o -e /data/activemq.log && ls -Falk /data/activemq.log || {
   ln -vsf /proc/self/fd/1 /data/activemq.log || return
 }
+
+rm -f \
+/usr/local/apache-activemq/conf/jetty-realm.properties \
+/usr/local/apache-activemq/conf/users.properties
+
 exec activemq-nowrapper
