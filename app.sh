3beec7170f9582cc61ec21e8093c2d8c5d4a0c5e
Elifarley C <elifarley@users.noreply.github.com>
2016-07-13 17:24:38 -0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 test -L /data/activemq.log -o -e /data/activemq.log && ls -Falk /data/activemq.log || {
-  ln -sf /proc/self/fd/1 /data/activemq.log || return
+  ln -vsf /proc/self/fd/1 /data/activemq.log || return
 }
 exec activemq-nowrapper
