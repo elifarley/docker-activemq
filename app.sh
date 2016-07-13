946cbc3cd1df89132a2d43d2df06be93a0a7bb10
Elifarley C <elifarley@users.noreply.github.com>
2016-07-13 16:52:49 -0300

    Test if /data/activemq.log is a symlink

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-test -e /data/activemq.log && ls -Falk /data/activemq.log || {
+test -L /data/activemq.log -o -e /data/activemq.log && ls -Falk /data/activemq.log || {
   ln -vs /dev/console /data/activemq.log || return
 }
 exec activemq-nowrapper
