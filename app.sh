23cb7193ab6080d2f645aeb3cc7921aa730e4480
Elifarley C <elifarley@users.noreply.github.com>
2016-07-20 19:30:04 -0300

    Use env var "CEP_LOG_FILES" (#1)
    Use env var "CEP_LOG_FILES" (elifarley/container-entrypoint#4)

--- app.sh
+++ app.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-test -L /data/activemq.log -o -e /data/activemq.log && ls -Falk /data/activemq.log || {
-  ln -vsf /proc/self/fd/1 /data/activemq.log || return
-}
-
 (cd /usr/local/apache-activemq/conf && rm -f jetty-realm.properties users.properties)
 
 exec activemq-nowrapper
